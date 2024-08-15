910cc6d9c0a2032feebea1ca19a57158f9213682
vciscato <vr.ciscato@gmail.com>
2024-08-15 12:08:11 -0300

    NomePopUp

--- Internet.ps1
+++ Internet.ps1
@@ -13,7 +13,7 @@ $webClient.DownloadFile($imageUrl, $tempImagePath)
 
 # Criar o formulário
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "Mensagem da CIPA"
+$form.Text = "Mensagem da CIPA - ATENÇÃO!"
 $form.Size = New-Object System.Drawing.Size(500, 400)
 $form.StartPosition = "CenterScreen"
 
